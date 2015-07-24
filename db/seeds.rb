699153b380f0632376faaa1c8e758ae9868f4044
Prity Patel <pritypatel714@gmail.com>
2015-07-24 11:28:25 -0700

    adding in music video list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+id: 1,
+  title: "In Style",
+  video_embed_uri: "https://www.youtube.com/watch?v=-CmadmM5cOk",
+  band_id: 1,
+  created_at: Fri, 24 Jul 2015 17:08:28 UTC +00:00,
+  updated_at: Fri, 24 Jul 2015 17:08:28 UTC +00:00>,
+ #<MusicVideo:0x007f8eff6b2ef8
+  id: 2,
+  title: "I Love Rock N Roll",
+  video_embed_uri: "https://www.youtube.com/watch?v=M3T_xeoGES8",
+  band_id: 2,
+  created_at: Fri, 24 Jul 2015 17:14:04 UTC +00:00,
+  updated_at: Fri, 24 Jul 2015 17:14:04 UTC +00:00>]
+
+   [#<Band:0x007f8eff4bb398
+  id: 1,
+  name: "Taylor Swift",
+  genre: "Pop",
+  created_at: Fri, 24 Jul 2015 16:55:31 UTC +00:00,
+  updated_at: Fri, 24 Jul 2015 16:55:31 UTC +00:00>,
+ #<Band:0x007f8eff4bb190
+  id: 2,
+  name: "Joan Jett",
+  genre: "Rock",
+  created_at: Fri, 24 Jul 2015 16:57:07 UTC +00:00,
+  updated_at: Fri, 24 Jul 2015 16:57:07 UTC +00:00>]
