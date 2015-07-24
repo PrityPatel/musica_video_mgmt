2798906cc9ff8936736b96cf9e5c4fbcd4101b1a
Prity Patel <pritypatel714@gmail.com>
2015-07-24 10:05:14 -0700

    updated gemfile with pry-rails and updated db MusicVideos migration file with uri instead of url

--- db/migrate/20150724162945_create_music_videos.rb
+++ db/migrate/20150724162945_create_music_videos.rb
@@ -2,7 +2,7 @@ class CreateMusicVideos < ActiveRecord::Migration
   def change
     create_table :music_videos do |t|
       t.string :title
-      t.string :video_embed_url
+      t.string :video_embed_uri
 
       t.integer :band_id
 
--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20150724162945) do
 
   create_table "music_videos", force: :cascade do |t|
     t.string   "title"
-    t.string   "video_embed_url"
+    t.string   "video_embed_uri"
     t.integer  "band_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
